9976dc91665fe47048b535a82b7f8eaecf166032
woXrooX <woXrooX@gmail.com>
2024-10-09 11:06:16 +0500

    More rules added

--- source/Markdown_to_HTML.js
+++ source/Markdown_to_HTML.js
@@ -1,5 +1,14 @@
-export default function Markdown_to_HTML(markdown){
+export default function Markdown_to_HTML(markdown) {
+	if (typeof markdown !== 'string') return '';
+
 	let HTML = markdown;
+
+	// Escape special characters
+	HTML = HTML.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&#039;');
 
 	// Convert headers
 	HTML = HTML.replace(/^# (.*$)/gm, '<h1>$1</h1>');
@@ -13,11 +22,46 @@ export default function Markdown_to_HTML(markdown){
 	HTML = HTML.replace(/\*(.*?)\*/g, '<em>$1</em>');
 
 	// Convert unordered lists
-	HTML = HTML.replace(/^\s*-\s(.*)$/gm, '<li>$1</li>');
-	HTML = HTML.replace(/(<li>.*<\/li>)/s, '<ul>$1</ul>');
+	HTML = HTML.replace(/^(- .+(\n|$))+/gm, function(match) {
+	  return '<ul>\n' + match.replace(/^- (.+)$/gm, '  <li>$1</li>') + '</ul>';
+	});
+
+	// Convert ordered lists
+	HTML = HTML.replace(/^(\d+\. .+(\n|$))+/gm, function(match) {
+	  return '<ol>\n' + match.replace(/^\d+\. (.+)$/gm, '  <li>$1</li>') + '</ol>';
+	});
+
+	// Convert links
+	HTML = HTML.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
+
+	// Convert line breaks
+	HTML = HTML.replace(/\\$/gm, '<br>');
 
 	// Convert paragraphs
 	HTML = HTML.replace(/^(?!<[uh]).*$/gm, '<p>$&</p>');
 
 	return HTML;
 }
+
+
+/////// Sample MDs
+`
+# H1
+## H2
+### H3
+
+*italic*
+**bold**
+***bold_italic***
+
+- UL 1
+- UL 2
+
+\ <br>
+
+1. OL 1
+2. OL 2
+
+Check out [woXrooX's website](https://www.woXrooX.com).
+
+`
